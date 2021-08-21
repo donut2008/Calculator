a21ccc9fcd4f0f11a3029c8c6dae0a39192c2519
defaultuser0 <76439683+donut2008@users.noreply.github.com>
2021-08-21 18:42:03 +0530

    Removed executable, updated source
    Removed executable for people to build their own fork

--- Calculator/Source.cpp
+++ Calculator/Source.cpp
@@ -1,14 +1,31 @@
 #include<iostream>
+#include<math.h>
+#include<conio.h>
 using namespace std;
 int main()
 {
 	int ch, a, b, sum = 0, prd = 0, quo = 0, rem = 0, res = 0;
-	cout << "Simple calculator v0.1 beta\n\n";
-	cout << "\nPlease note that this calculator ONLY supports operations like a+b,a*b,etc!\n";
-	cout << "\nEnter two numbers: ";
-	cin >> a >> b;
-	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n";
-	LOOP:cout << "\nEnter your choice: ";
+	cout << "Simple calculator v0.2 alpha\n\n";
+	cout << "\nNote about this calculator: Currently only supports operations like a+b, a-b, a*b etc.\n\n";
+INPUT:cout << "Enter two numbers: ";
+	cin >> a;
+	if (!cin)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Please enter two valid numbers.\n\n";
+		goto INPUT;
+	}
+	cin >> b;
+	if (!cin)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Please enter two valid numbers.\n\n";
+		goto INPUT;
+	}
+	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n6. Square root\n";
+LOOP:cout << "\nEnter your choice: ";
 	cin >> ch;
 	if (ch == 1)
 	{
@@ -35,10 +52,17 @@ int main()
 		prd = a * b;
 		cout << "Result = " << prd << endl;
 	}
+	else if (ch == 6)
+	{
+		cout << "Square root of " << a << " is " << sqrt(a) << endl;
+		cout << "Square root of " << b << " is " << sqrt(b);
+	}
 	else
 	{
 		cout << "Invalid choice, please enter a valid choice.\n";
 		goto LOOP;
 	}
+	cout << "Press any key to exit.";
+	_getch();
 	return 0;
 }
