8043e12fead032f7d4a6b7f885d4c8f4ac4e2c0a
defaultuser0 <76439683+donut2008@users.noreply.github.com>
2021-08-21 22:23:14 +0530

    Update Source.cpp

--- Calculator/Source.cpp
+++ Calculator/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	int ch, a, b, sum = 0, prd = 0, quo = 0, rem = 0, res = 0;
-	cout << "Simple calculator v0.2 alpha\n\n";
+	cout << "Simple calculator v0.2.1 beta\n\n";
 	cout << "\nNote about this calculator: Currently only supports operations like a+b, a-b, a*b etc.\n\n";
 INPUT:cout << "Enter two numbers: ";
 	cin >> a;
@@ -24,38 +24,47 @@ INPUT:cout << "Enter two numbers: ";
 		cout << "Please enter two valid numbers.\n\n";
 		goto INPUT;
 	}
-	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n6. Square root\n";
+	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n6. Square root\n7. Cube root\n8. Exponent (a^b)\n";
 LOOP:cout << "\nEnter your choice: ";
 	cin >> ch;
 	if (ch == 1)
 	{
 		sum = a + b;
-		cout << "Result = " << sum << endl;
+		cout << "Result = " << sum << endl << endl;
 	}
 	else if (ch == 2)
 	{
 		res = a - b;
-		cout << "Result = " << res << endl;
+		cout << "Result = " << res << endl << endl;
 	}
 	else if (ch == 3)
 	{
 		quo = a / b;
-		cout << "Result = " << quo << endl;
+		cout << "Result = " << quo << endl << endl;
 	}
 	else if (ch == 4)
 	{
 		rem = a % b;
-		cout << "Result = " << rem << endl;
+		cout << "Result = " << rem << endl << endl;
 	}
 	else if (ch == 5)
 	{
 		prd = a * b;
-		cout << "Result = " << prd << endl;
+		cout << "Result = " << prd << endl << endl;
 	}
 	else if (ch == 6)
 	{
 		cout << "Square root of " << a << " is " << sqrt(a) << endl;
 		cout << "Square root of " << b << " is " << sqrt(b);
+	}
+	else if (ch == 7)
+	{
+		cout << "Cube root of " << a << " is " << cbrt(a) << endl;
+		cout << "Cube root of " << b << " is " << cbrt(b) << endl << endl;
+	}
+	else if (ch == 8)
+	{
+		cout << "Result = " << pow(a, b) << endl << endl;
 	}
 	else
 	{
