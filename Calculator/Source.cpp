00c877e19fedf3ba60157a1fe0a41ecc00f43cd3
defaultuser0 <76439683+donut2008@users.noreply.github.com>
2021-08-24 20:40:28 +0530

    Update Source.cpp

--- Calculator/Source.cpp
+++ Calculator/Source.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	int ch, a, b, sum = 0, prd = 0, quo = 0, rem = 0, res = 0, fact = 1, fbct = 1;
+	int ch, a, b, sum = 0, prd = 0, quo = 0, rem = 0, res = 0;
 	cout << "Simple calculator v0.3 beta\n\n";
 	cout << "\nThis calculator only supports operations in a/b format.\n\n";
 INPUT:cout << "Enter two numbers: ";
@@ -24,7 +24,7 @@ INPUT:cout << "Enter two numbers: ";
 		cout << "Please enter two valid numbers.\n\n";
 		goto INPUT;
 	}
-	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n6. Square root\n7. Cube root\n8. Exponent (a^b)\n9. Factorial\n";
+	cout << "\n1. Add\n2. Subtract\n3. Quotient\n4. Remainder\n5. Product\n6. Square root\n7. Cube root\n8. Exponent (a^b)\n";
 LOOP:cout << "\nEnter your choice: ";
 	cin >> ch;
 	if (ch == 1)
@@ -64,34 +64,6 @@ LOOP:cout << "\nEnter your choice: ";
 	}
 	else if (ch == 8)
 		cout << "Result = " << pow(a, b) << endl << endl;
-	else if (ch == 9)
-	{
-		if (a < 0 && b < 0)
-			cout << "Factorials for negative numbers don't exist!";
-		else if (a > 0 && b < 0)
-		{
-			for (int ia = 1; ia <= a; ia++)
-				fact = fact * ia;
-			cout << "Factorial of " << a << " = " << fact << endl;
-			cout << "Factorial for " << b << " doesn't exist because it is a negative number." << endl << endl;
-		}
-		else if (a < 0 && b>0)
-		{
-			for (int ib = 1; ib <= b; ib++)
-				fbct = fbct * ib;
-			cout << "Factorial of " << b << " = " << fbct << endl;
-			cout << "Factorial for " << a << " doesn't exist because it is a negative number." << endl << endl;
-		}
-		else
-		{
-			for (int ia = 1; ia <= a; ia++)
-				fact = fact * ia;
-			for (int ib = 1; ib <= b; ib++)
-				fbct = fbct * ib;
-			cout << "Factorial of " << a << " = " << fact << endl;
-			cout << "Factorial of " << b << " = " << fbct << endl << endl;
-		}
-	}
 	else
 	{
 		cout << "Invalid choice, please enter a valid choice.\n";
