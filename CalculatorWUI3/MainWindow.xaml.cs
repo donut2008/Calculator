d7e704c193ce69c1ba450e948bc778cb71188c5c
donut2008 <76439683+donut2008@users.noreply.github.com>
2021-12-11 19:45:28 +0530

    winui 3 is ok i guess

---
+++ CalculatorWUI3/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CalculatorWUI3
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        private void navigator_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = (NavigationViewItem)args.SelectedItem;
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(settings), null);
+            }
+            else
+            {
+                string selectedItemTag = selectedItem.Tag.ToString();
+                switch (selectedItemTag)
+                {
+                    case "calc":
+                        ContentFrame.Navigate(typeof(buttons), null);
+                        break;
+                    case "temp":
+                        ContentFrame.Navigate(typeof(temperature), null);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
